583ebf8d36df2c040b0f68d7434a79be96362d25
Sankalp Heranjal <hsankalp@gmail.com>
2020-02-16 10:55:14 -0500

    Updates

---
+++ precache-manifest.ffa20d6c31e3d55f4e6ff55edd1b93a6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19d0c8bc95fc436b7897909bf01fc216",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9bec602410bab54b2e11",
+    "url": "/portfolio/static/css/main.110f864f.chunk.css"
+  },
+  {
+    "revision": "1483a7e4e85e977a2ec3",
+    "url": "/portfolio/static/js/2.9b8616ae.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/portfolio/static/js/2.9b8616ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bec602410bab54b2e11",
+    "url": "/portfolio/static/js/main.80eafde5.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  },
+  {
+    "revision": "d5d94dd1480eff6b93771533b1dd8b40",
+    "url": "/portfolio/static/media/home-profile.d5d94dd1.PNG"
+  },
+  {
+    "revision": "ccbb71dea3465c6d38b1259143cf8153",
+    "url": "/portfolio/static/media/resume-profile.ccbb71de.PNG"
+  },
+  {
+    "revision": "84d702bf32c6aab841a538b606eb8dba",
+    "url": "/portfolio/static/media/resume.84d702bf.pdf"
+  }
+]);
