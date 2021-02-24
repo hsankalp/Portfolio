2bfc0e541ebfd4188d79be8a54412f540083848a
Sankalp Heranjal <hsankalp@gmail.com>
2021-02-24 11:26:23 -0500

    Fixed resume path

--- src/components/About/Bio.jsx
+++ src/components/About/Bio.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Grid, Typography, Button } from "@material-ui/core";
 import { Phone, GetApp, Mail } from "@material-ui/icons";
 import profileImage from "../../images/resume-profile.PNG";
-import resume from "../../resume/resume.pdf";
+import resume from "../../resume/SankalpHeranjalResume.pdf";
 import IconLabel from "../IconLabel";
 import { makeStyles } from "@material-ui/core/styles";
 
